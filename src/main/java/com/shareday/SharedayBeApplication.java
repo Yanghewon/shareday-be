6676ddabfcf98467e15aabe00e71dbc9b5a0ab15
Yanghewon <hewon8883@naver.com>
2025-09-09 20:29:41 +0900

    Initial commit: Add project files and setup

---
+++ src/main/java/com/shareday/SharedayBeApplication.java
@@ -0,0 +1,13 @@
+package com.shareday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedayBeApplication.class, args);
+	}
+
+}

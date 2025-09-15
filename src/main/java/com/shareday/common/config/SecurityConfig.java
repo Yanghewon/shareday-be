c36ad867a0abb6be13f047c806e35260fc5cb381
ch <cksghrha@naver.com>
2025-09-15 14:27:01 +0900

    feat: enum validator 어노테이션 추가

--- src/main/java/com/shareday/common/config/SecurityConfig.java
+++ src/main/java/com/shareday/common/config/SecurityConfig.java
@@ -23,14 +23,19 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService) // 주입받은 빈 사용
-                        )
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
                 );
+
         return http.build();
     }
 }
---
+++ src/main/java/com/shareday/common/validation/EnumValidator.java
@@ -0,0 +1,25 @@
+package com.shareday.common.validation;
+
+import com.shareday.common.annotaion.ValidEnum;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Arrays;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, String> {
+
+    private Class<? extends Enum<?>> enumClass;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.enumClass = constraintAnnotation.enumClass();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) return true; // null은 다른 @NotNull 로 처리
+
+        return Arrays.stream(enumClass.getEnumConstants())
+                .anyMatch(e -> e.name().equalsIgnoreCase(value));
+    }
+}

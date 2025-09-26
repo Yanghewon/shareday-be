356434bc08ce2192466ee180e9f71ab922679bc1
Yanghewon <hewon8883@naver.com>
2025-09-26 23:31:18 +0900

    feat: event entity 및 API에 isDday 필드 추가

--- src/main/java/com/shareday/event/dto/EventResponse.java
+++ src/main/java/com/shareday/event/dto/EventResponse.java
@@ -12,5 +12,6 @@ public record EventResponse(
         LocalDate eventDate,
         LocalDateTime startTime,
         LocalDateTime endTime,
-        ParticipantType participantType
+        ParticipantType participantType,
+        Boolean isDday
 ) {}

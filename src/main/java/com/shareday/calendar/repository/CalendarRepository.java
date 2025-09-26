5cf80936b9f96dad68f35d9201a6ce893008834b
Yanghewon <hewon8883@naver.com>
2025-09-26 13:07:35 +0900

    feat : calendar : 캘린더 기능 추가

--- src/main/java/com/shareday/calendar/repository/CalendarRepository.java
+++
@@ -1,6 +0,0 @@
-package com.shareday.calendar.repository;
-
-import com.shareday.calendar.entity.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {}
